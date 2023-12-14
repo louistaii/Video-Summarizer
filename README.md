75cad2c9f28cb8679548b7982db2dbbbcb87e2ca
Louis Tai <142078052+louistaii@users.noreply.github.com>
2023-12-14 18:03:15 +0800

    update 141223

--- README.md
+++ README.md
@@ -1,16 +1,28 @@
 # Video Summarizer
+Transcribe and summarize local/ downloaded videos for faster consumption of information.
+Ceated to summarize Abdul Bari YouTube lectures because playing on 2x speed isn't for everyone
 
-## Description  
-- Created to summarize Abdul Bari YouTube lectures because playing on 2x speed isn't for everyone
-- Download YouTube videos using Pytube or load local videos                                                                                                     
-- Transcribes and summarizes video content using OpenAI API                                                                                                                       
+## Notes
+- Tested on Python 3.11  
+- Overall estimated run time: 2mins for a 30min video
+- Python modules needed: [Pytube](https://pytube.io/en/latest/), [Pydub](https://github.com/jiaaro/pydub), [Openai](https://github.com/openai/openai-python)
+- Downloading of YouTube video audio using Pytube                                                                                                  
+- Longer audio inputs are split and processed using Pydub
+- Transcription and summarry done using OpenAI API (gpt-3.5-turbo-1106)                                                                                               
 - Get your own [API KEY](https://openai.com/pricing) and paste into "API_KEY" file
-- Tested on Python 3.11
 
 
 ## Updates
 131223
 - Loading of local videos
 - Splitting of longer audio files using Pydub to enable transcription of longer videos
-- Tested up to audio length: 1hr 20mins
-- Openai API unable to process and summarise large text (videos with extensive transcription)
+- Testing of transcription up to audio length: 1hr 20mins
+- gpt-3.5-turbo-1106 unable to process videos with extensive transcription (use gpt 4 instead)
+- Testing of summary with gpt-3.5-turbo-1106 up to audio length: 45mins
+
+141223
+- Saving of summary
+- Added progress bar and indicators
+- Fixed bugs removing audio files to reduce cluster
+- Improved openai prompt
+- Testing of invalid urls
--- summary.py
+++ summary.py
@@ -1,9 +1,10 @@
 
 import os               
 import math
+from urllib.parse import urlparse
 
-#run pip install on openai, pytube and pydub
-import openai
+#external python modules
+import openai                   #API
 from pytube import YouTube      #downloads audio off youtube
 from pydub import AudioSegment  #for audio file manipulation
 
@@ -12,25 +13,34 @@ API_KEY = open("API_KEY", "r").read()
 openai.api_key = API_KEY
 
 
+def progress_bar(current, total):                #shows progress 
+    percent = 100 * (current/ float(total))
+    bar ='█' * int(percent) + '-' * (100 - int(percent))
+    print(f"\r|{bar}| {percent:.2f}%", end = "\r")
+
+
+
 def summarize():
     lines = open("transcription.txt", "r", encoding="utf-8").read()   #encoding utf-8 used to prevent errors with openai
-    print("Summarising...\n")
+    print("\nSummarising...\n")
     
     response = openai.chat.completions.create(           #openai chat completion api
-        model="gpt-3.5-turbo-1106",
+        model="gpt-3.5-turbo-1106",                      #use gpt-4 for long transcriptions
         messages= [
-            {"role": "system", "content": "You are a research assistant that summarises the given text into short yet detailed notes. Prioritize including statistics, formulas and equations. Ignore advertisements"},
+            {"role": "system", "content": "Act as a text summarizer to create a concise summary of the text provided. Express the key points and concepts written in the original text without adding your interpretations. Ignore advertisements"},
             {"role": "user", "content": lines}]
             )
     
     output = response.choices[0].message.content.strip() #stripping response from api into readible format
     print(output)
 
+    with open("summary.txt", "w" , encoding="utf-8") as f: 
+        f.write(output)
+
 
 
 def converttotext(audiodir):
     audio_file = open(audiodir, "rb")                   #loads mp3 file
-    print("Transcribing...")
     transcript = openai.audio.translations.create(      #transcibe/translate with openai API
         model="whisper-1", 
         file=audio_file, 
@@ -40,51 +50,76 @@ def converttotext(audiodir):
         f.write(transcript)
 
 
-def splitaudio(audio, audiolength):                     #split audio file into files of 20mins each
-    splits = math.ceil(audiolength / (1200))
+def splitaudio(audio, splits):                     #split audio file into files of 20mins each
+
+    #erase previous transcription.txt
+    if os.path.exists("transcription.txt"):
+        os.remove("transcription.txt")
+
+
+    #splits audio into 20min (1200s) audio files
+    progress_bar(0, splits)
     for i in range(splits):
-        print(f"Splitting ({i+1} / {splits})...")
         start = i * 1200 * 1000
         end = (i + 1) * 1200 * 1000
         split_audio = audio[start:end]
         output_path = os.path.join(f"part{i+1}.mp3")
-        split_audio.export(output_path, format="mp3")
-        print("Completed")                              
+        split_audio.export(output_path, format="mp3")                             
         converttotext(output_path)                      #transcribing individual smaller audio file 
-        if os.path.exists(output_path):                 #delete smaller audio file to prevent clutter
-            os.remove(output_path)
+        progress_bar(i+1, splits)
+
+
+
+def uri_validator(link):        #tests for valid url
+    try:
+        result = urlparse(link)
+        return all([result.scheme, result.netloc])
+    except:
+        return False
 
 
 
 def main():
-    
+
     link = input('Insert video directory/link: ')
-    
+   
     #fetch audio file
     if os.path.exists(link):
         audiodir = link
-    else:
+    elif uri_validator(link) == True:
         yt =  YouTube(link)
         t=yt.streams.filter(only_audio=True)
         print("\nDownloading audio...")
         t[0].download(filename="audio.mp3")
         audiodir = "audio.mp3"
-    
-    #erase previous transcription.txt
-    if os.path.exists("transcription.txt"):
-        os.remove("transcription.txt")
-
-    audio = AudioSegment.from_file(audiodir)
-    audiolength = audio.duration_seconds
-
-    #processing lengthy audio files
-    if (audiolength >1800):
-        print(f"Audio file needs splitting: {audiolength}s")
-        splitaudio(audio,audiolength)
     else:
-        converttotext(audiodir)
+        print("\nInvalid file directory or YouTube link")
+        quit()
+        
     
+    audio = AudioSegment.from_file(audiodir)
+    audiolength = math.ceil(audio.duration_seconds)
+    splits = math.ceil(audiolength / (1200))
+
+
+
+    #processing lengthy audio files (>30mins)
+    if (audiolength >1800):
+        print(f"Splitting file (estimate: {splits} mins)...")
+        splitaudio(audio,splits)
+    else:
+        print("Transcribing...")
+        converttotext(audiodir)
+
     summarize()
+
+    #remove splitted mp3 files to reduce clutter
+    for i in range(splits):
+        if os.path.exists(f"part{i+1}.mp3"):
+            os.remove((f"part{i+1}.mp3"))
+        else:
+            quit
+
 
 
 
